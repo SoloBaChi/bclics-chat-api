a6bd1eb59d994cbeeb6adec850492469e012e00d
SoloBaChi <solobachi02@gmail.com>
2025-04-03 11:42:26 +0100

    added some controller logic

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import morgan from "morgan";
 import dotenv from "dotenv"
+import { ResponseMessage } from "./utils/responseMessage.js";
 
 
 
@@ -10,10 +11,7 @@ dotenv.config()
 
 const app = express();
 
-
-
 //custom middlewares
-// app.use(())
 app.use(express.json()); // Parse incoming JSON requests
 app.use(morgan("dev"));
 app.use(cors({ origin: ["https://www.bclics.com",["https://bclicscom.vercel.app"],"http://localhost:5175"], credentials: true }));
@@ -21,7 +19,35 @@ app.disable("x-powered-by"); // Hide server stack details
 
 
 
-const PORT = process.env.PORT || 4001;
+
+//Defualt Route
+app.get("/", (req, res) => {
+  return res.status(200).json(new ResponseMessage("success",200,`Welcome to ${process.env.APP_NAME}`));
+});
+
+
+// Default error handling
+app.use((err, req, res, next) => {
+    console.error(err.stack); // Log error stack in development
+    res.status(err.status || 500).json({
+      status: "error",
+      statusCode: err.status || 500,
+      message: err.message || "Internal Server Error",
+    });
+  });
+  
+  // NOT FOUND ROUTE
+  app.use("*", (req, res) => {
+    res.status(400).json({
+      status: "error",
+      statusCode: 400,
+      message: "Wrong Route",
+    });
+  });
+
+
+  const PORT = process.env.PORT || 4001;
+
 
 export const start = () => {
  app.listen(PORT,()=>{
